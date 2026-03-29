17aa34ced04f2419d79a96684b1dedbb97d91aa0
Deltoro123 <100856450+Deltoro123@users.noreply.github.com>
2026-03-29 17:09:35 +0500

    Add files via upload

---
+++ Labs5_Exercise1_MakarovDO/Labs5_Exercise1_MakarovDO/Labs5_Exercise1_MakarovDO.cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main(int argc, char* argv[])
+{
+    char ch;
+    FILE* in;
+
+    // Устанавливаем русскую локаль для корректного вывода
+    setlocale(LC_ALL, "Russian");
+    // Альтернатива: setlocale(LC_ALL, "ru-RU");
+    // Или: setlocale(LC_ALL, ".1251"); для Windows-1251
+
+    // Если имя файла передано как аргумент командной строки
+    if (argc > 1) {
+        if (fopen_s(&in, argv[1], "r") != 0) {
+            printf("Файл %s не открыт\n", argv[1]);
+            return 1;
+        }
+    }
+    else {
+        // Запрос имени файла у пользователя
+        char name[50] = { 0 };
+        printf("Введите имя файла для просмотра: ");
+
+        // Безопасный ввод с ограничением длины
+        if (scanf_s("%49s", name, (unsigned)sizeof(name)) != 1) {
+            printf("Ошибка ввода имени файла\n");
+            return 1;
+        }
+
+        if (fopen_s(&in, name, "r") != 0) {
+            printf("Файл %s не открыт\n", name);
+            return 1;
+        }
+    }
+
+    // Вывод содержимого файла
+    while ((ch = fgetc(in)) != EOF) {
+        putchar(ch);
+    }
+
+    fclose(in);
+    return 0;
+}
---
+++ Labs5_Exercise2_MakarovDO/Labs5_Exercise2_MakarovDO/Labs5_Exercise2_MakarovDO.cpp
@@ -0,0 +1,52 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <ctype.h>
+#include <windows.h>
+
+int main(int argc, char* argv[])
+{
+    char ch;
+    FILE* in;
+    int empty_count = 0;
+    int non_empty_count = 0;
+    char name[50] = { 0 };
+
+    // Устанавливаем кодировку консоли Windows-1251
+    SetConsoleCP(1251);      // Для ввода
+    SetConsoleOutputCP(1251); // Для вывода
+
+    printf("Введите имя файла для просмотра: ");
+
+    if (scanf("%49s", name) != 1) {
+        printf("Ошибка ввода имени файла\n");
+        return 1;
+    }
+
+    if ((in = fopen(name, "r")) == NULL) {
+        printf("Файл %s не открыт\n", name);
+        return 1;
+    }
+
+    printf("\nСодержимое файла:\n");
+    printf("-----------------\n");
+
+    while ((ch = fgetc(in)) != EOF) {
+        putchar(ch);
+
+        if (isspace(ch) || iscntrl(ch)) {
+            empty_count++;
+        }
+        else {
+            non_empty_count++;
+        }
+    }
+
+    printf("\n-----------------\n");
+    printf("\nРезультаты подсчета:\n");
+    printf("Пустых символов: %d\n", empty_count);
+    printf("Непустых символов: %d\n", non_empty_count);
+    printf("Всего символов: %d\n", empty_count + non_empty_count);
+
+    fclose(in);
+    return 0;
+}
